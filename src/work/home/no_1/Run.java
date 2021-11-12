70d2268681ee9f000d499bb76a86de7fffbd10cc
anstnwjd22 <anstnwjd22@naver.com>
2021-11-12 23:38:26 +0900

    second commit

---
+++ src/work/home/no_1/Run.java
@@ -0,0 +1,16 @@
+package work.home.no_1;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		VariablePractice1 vp = new VariablePractice1();
+		
+//		vp.question1();
+//		vp.question2();
+//		vp.question3();
+		vp.question4();
+
+	}
+
+}
